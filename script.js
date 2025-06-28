c2d9a300de22d3a5c0178e65f86ab7aaab7d03d8
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-06-27 23:45:02 -0400

    Implemented Player object, GameRounds object, checkWinner function
    - Updated gameboard to be 1D array instead of 2D for easier implementation of checkWinner function
    - Updated gameboard to initiate with null and created init function to do so
    - Implemented checkWinner function to check for winner
    - Implemented Player object and GameRounds object

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
-const Gameboard = (function () {
+const GameBoard = (function () {
   let gameboard;
   const init = function () {
-    gameboard = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ];
+    gameboard = [null, null, null, null, null, null, null, null, null];
   };
-  const updateGameboard = function (i1, i2, value) {
-    gameboard[i1][i2] = value;
+
+  const checkWinner = function () {
+    winningCombos.some((combo) => {
+      const first = gameboard[combo[0]];
+      if (first == null) {
+        return false;
+      }
+      return combo.every((index) => gameboard[index] === first);
+    });
+  };
+
+  const updateGameboard = function (i, value) {
+    if (i <= 8) {
+      gameboard[i] = value;
+    }
   };
 
   init();
-
   return { updateGameboard: updateGameboard, init: init };
+})();
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const Player = function (name) {
+  let playerName = name;
+  let points = 0;
+  const incrementPoints = function () {
+    this.points = ++points;
+  };
+  return {
+    name: playerName,
+    points: points,
+    incrementPoints: incrementPoints,
+  };
+};
+
+(function () {
+  const GameRounds = {
+    players: [],
+    init: function () {
+      this.functions();
+    },
+    functions: function () {
+      this.createPlayer = function (name) {
+        if (this.players.length < 2) {
+          this.players.push(new Player(name));
+        } else {
+          throw Error("More than 2 players created");
+        }
+      };
+      this.resetBoard = function () {
+        GameBoard.init();
+      };
+    },
+  };
+
+  GameRounds.init();
 })();
